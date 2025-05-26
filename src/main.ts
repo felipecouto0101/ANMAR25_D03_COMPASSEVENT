6ec626de04366d59eecbdf35dab82909806188a8
Felipe Couto <mto.companhia@gmail.com>
2025-05-26 19:36:32 -0300

    feat: add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
   
   const config = new DocumentBuilder()
     .setTitle('ANMAR25_D03_COMPASSEVENT')
