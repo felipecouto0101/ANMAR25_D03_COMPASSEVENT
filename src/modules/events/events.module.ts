f3a2a6cc250ecaab612c5a39b38b13b1c9019147
Felipe Couto <mto.companhia@gmail.com>
2025-05-26 19:36:23 -0300

    feat: integrate S3 module with events module

--- src/modules/events/events.module.ts
+++ src/modules/events/events.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../../infrastructure/database/database.module';
+import { S3Module } from '../../infrastructure/storage/s3/s3.module';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
 import { EventDynamoDBRepository } from '../../infrastructure/repositories/event.repository';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, S3Module],
   controllers: [EventsController],
   providers: [
     EventsService,
