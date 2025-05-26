04e32740c04b3f4194f1393a5c5ac496b18c0b4e
Felipe Couto <mto.companhia@gmail.com>
2025-05-26 18:09:27 -0300

    feat: integrate database and config modules into app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { DatabaseModule } from './infrastructure/database/database.module';
+import { EventsModule } from './modules/events/events.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule,
+    DatabaseModule,
+    EventsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
