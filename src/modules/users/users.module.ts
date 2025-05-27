2d7942255f6e719f32157b2e3a8ab38e14647021
Felipe Couto <mto.companhia@gmail.com>
2025-05-26 21:06:59 -0300

    feat: configure users module with dependencies

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../../infrastructure/database/database.module';
+import { S3Module } from '../../infrastructure/storage/s3/s3.module';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 import { UserDynamoDBRepository } from '../../infrastructure/repositories/user.repository';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, S3Module],
+  controllers: [UsersController],
   providers: [
     UsersService,
     {
