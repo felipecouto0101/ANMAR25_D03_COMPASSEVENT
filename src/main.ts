efc5fea9e55961d0345b54d76df213aeccdede1c
Felipe Couto <mto.companhia@gmail.com>
2025-05-27 20:52:26 -0300

    feat: configure global exception filter and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './infrastructure/filters/exception.filter';
+import { CustomValidationPipe } from './infrastructure/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-    whitelist: true,
-    forbidNonWhitelisted: true,
-  }));
+  app.useGlobalPipes(new CustomValidationPipe());
+  app.useGlobalFilters(new AllExceptionsFilter());
+  
+  app.enableCors();
   
   const config = new DocumentBuilder()
     .setTitle('ANMAR25_D03_COMPASSEVENT')
