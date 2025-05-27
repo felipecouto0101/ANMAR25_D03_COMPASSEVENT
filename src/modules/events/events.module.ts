494301c05019c6b270f62ff575efbdf93e7dc2fc
Felipe Couto <mto.companhia@gmail.com>
2025-05-27 20:51:29 -0300

    refactor: update modules with proper dependency injection

--- src/modules/events/events.module.ts
+++ src/modules/events/events.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../../infrastructure/database/database.module';
 import { S3Module } from '../../infrastructure/storage/s3/s3.module';
 import { MailModule } from '../../infrastructure/mail/mail.module';
+import { ConfigModule } from '@nestjs/config';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
 import { EventDynamoDBRepository } from '../../infrastructure/repositories/event.repository';
 import { UserDynamoDBRepository } from '../../infrastructure/repositories/user.repository';
 
 @Module({
-  imports: [DatabaseModule, S3Module, MailModule],
+  imports: [DatabaseModule, S3Module, MailModule, ConfigModule],
   controllers: [EventsController],
   providers: [
     EventsService,
