62cc1305e78637dbb9ba3713cc2daa37a9b98177
Felipe Couto <mto.companhia@gmail.com>
2025-05-29 14:07:31 -0300

    test(database): add unit tests for database module

---
+++ src/infrastructure/database/database.module.spec.ts
@@ -0,0 +1,31 @@
+
+jest.mock('./dynamodb/dynamodb.module', () => {
+  const DynamoDBModuleMock = function() {};
+  DynamoDBModuleMock.register = jest.fn().mockReturnValue({
+    module: DynamoDBModuleMock,
+    providers: [],
+    exports: [],
+  });
+  
+  return {
+    DynamoDBModule: DynamoDBModuleMock,
+  };
+});
+
+
+import { Test, TestingModule } from '@nestjs/testing';
+import { DatabaseModule } from './database.module';
+
+describe('DatabaseModule', () => {
+  let module: TestingModule;
+
+  beforeEach(async () => {
+    module = await Test.createTestingModule({
+      imports: [DatabaseModule],
+    }).compile();
+  });
+
+  it('should be defined', () => {
+    expect(module).toBeDefined();
+  });
+});
